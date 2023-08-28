577d859789c4c021ffa87f6ace00309caf04556e
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-08-28 14:52:46 -0400

    configure env variables access in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import { Button } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
+	console.log('Hello');
+	console.log(import.meta.env.VITE_APP_SERVER_URL);
 	return (
 		<>
 			<CssBaseline />
