6b64ebbb7afe628c665eaa2bbb244f88083334a9
Konstantin Krumin <konstantin.krumin@gmail.com>
2023-08-29 16:35:21 -0400

    minor change to the dropdown

--- client/src/components/UI/Dropdown.tsx
+++ client/src/components/UI/Dropdown.tsx
@@ -1,6 +1,4 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
@@ -19,26 +17,17 @@ const Dropdown: React.FC<DropdownProps> = ({ input, questionInfo, onInputChange 
 	};
 
 	return (
-		<Box sx={{ minWidth: 120 }}>
-			<FormControl fullWidth>
-				<InputLabel id="select-label">{questionInfo.text}</InputLabel>
-				<Select
-					id="select"
-					labelId="select-label"
-					value={input}
-					// label="Age"
-					onChange={handleInputChange}
-				>
-					{questionInfo?.answerOptions?.map((answerOption, idx) => {
-						return (
-							<MenuItem key={idx} value={answerOption}>
-								{answerOption}
-							</MenuItem>
-						);
-					})}
-				</Select>
-			</FormControl>
-		</Box>
+		<FormControl fullWidth>
+			<Select id="select" labelId="select-label" value={input} onChange={handleInputChange}>
+				{questionInfo?.answerOptions?.map((answerOption, idx) => {
+					return (
+						<MenuItem key={idx} value={answerOption}>
+							{answerOption}
+						</MenuItem>
+					);
+				})}
+			</Select>
+		</FormControl>
 	);
 };
 
